d7c8f4562ffb7f1b000ae920fe3e38b31177e268
simranb82232 <simranb82232@gmail.com>
2022-07-11 15:20:21 -0700

    database connection

--- src/main/java/com/simranb/employeesystem/controller/EmployeeController.java
+++ src/main/java/com/simranb/employeesystem/controller/EmployeeController.java
@@ -1,5 +1,22 @@
 package com.simranb.employeesystem.controller;
 
-public class EmployeeController {
+import com.simranb.employeesystem.model.Employee;
+import com.simranb.employeesystem.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return "New student is added";
+    }
 }
